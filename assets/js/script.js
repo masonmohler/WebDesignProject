4ca7a117e3c8842e0b47d87b24138c51080dd10b
masonmohler <112035573+masonmohler@users.noreply.github.com>
2023-12-07 21:35:56 +0000

    finished structuring blog posts

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const posts = document.querySelectorAll(".blog-post");
-
-  posts.forEach((post) => {
-    post.addEventListener("click", function () {
-      this.querySelector(".full-post").classList.toggle("show");
-    });
-  });
-});
